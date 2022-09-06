020cc9713a65817d8c4e5545073dbca5fe1101ba
babbarparth <98263947+babbarparth@users.noreply.github.com>
2022-09-06 15:22:30 +0530

    d

--- src/NewSort.java
+++ src/NewSort.java
@@ -36,6 +36,7 @@ public class NewSort {
             for (int j = i+1; j < n; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
+            
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
